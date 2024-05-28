85c364248ec14426620387c66dfad4d8f2cb3369
rocketman510 <rocketman18354@gmail.com>
2024-05-28 00:36:45 -0700

    Added config.js error correction

--- index.js
+++ index.js
@@ -8,13 +8,55 @@ supervisorPermisses.on('fail', () => {
 if (supervisorPermissesBoolean) {
     let timmybot
     let chalk
+    let fs
     
     if (supervisorPermissesBoolean) {
         try {
-            chalk = require('fs')
+            fs = require('fs')
             supervisor.succeed('fs module successfully loaded')
         } catch (err) {
             supervisor.fail(1, err, 'fs module failed to load')
+        }
+    }
+
+    if (supervisorPermissesBoolean) {
+        const path = "timmybot v1.0/config.json"
+        const jsonObj = {
+            token: "",
+            aiChat: "",
+            aiCookieValue: "",
+            guildID: "",
+            verificationChannelID: "",
+            ruleChannelID: "",
+            supportChannelID: "",
+            aiChannelID: "",
+            webmasterRoleID: "",
+        }
+
+        function accessConif() {
+             try {
+                fs.accessSync(path, fs.constants.F_OK)
+                return true
+            } catch (error) {
+                return false
+            }   
+        }
+
+
+
+        try {
+            if (accessConif()) {
+                supervisor.succeed('config.json instated')
+            } else {
+                fs.writeFileSync(path, JSON.stringify(jsonObj, null, 2), 'utf8')
+                if (accessConif()) {
+                    supervisor.succeed('successful generated confit.json')
+                } else {
+                    supervisor.fail(1, "", 'failed to generate config.json')
+                }
+            }
+        } catch (err) {
+            supervisor.fail(1, err, 'failed to check for config.json')
         }
     }
 
