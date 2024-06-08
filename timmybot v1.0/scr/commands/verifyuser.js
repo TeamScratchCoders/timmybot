9aaee79376245d4c519a677d049d0c27b99ae941
rocketman510 <rocketman18354@gmail.com>
2024-06-08 09:53:51 -0700

    fixed bug with requiring verification.js

--- timmybot v1.0/scr/commands/verifyuser.js
+++ timmybot v1.0/scr/commands/verifyuser.js
@@ -1,8 +1,6 @@
 const verifyuser = async (i) => {
-    const verification = require('../functions/verification.js')
-    console.log(i.options._hoistedOptions[0].value)
-    console.log(verification);
     try {
+        const { verification } = require('../functions/verification.js')
         if (await verification.verifyUserByID(i.options._hoistedOptions[0].value)) {
             i.reply({ content: "Added user to verify members list.", flags: 64 })
         } else {
